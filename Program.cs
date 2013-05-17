87eb7db5056a156f423731638458bbea3cce8559
Thomas Trenta` <Thomas.Trenta@weatherford.com>
2013-05-17 11:33:00 -0500

    Changes Made buddy

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Writeln("Github is kinda cool!");
+            Console.WriteLine("Github is kinda cool!");
         }
     }
 }
